9d7a510379b453e4da8124fdc170535e2b877123
Amitabh <sonuambastha117@gmail.com>
2023-08-07 17:16:18 +0530

    google auth added

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const mongoose = require('mongoose');
 const session = require('express-session')
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate')
+
 const app = express();
 
 app.use(express.static('public'));
@@ -28,16 +31,50 @@ mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+
+
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      cb(null, { id: user.id, username: user.username });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+
+
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
+
+
 
 
 
@@ -45,13 +82,26 @@ app.get("/", function (req, res) {
     res.render('home')
 })
 
+
+app.get("/auth/google" ,
+    passport.authenticate("google", {scope: ["profile"]})
+);
+
+app.get("/auth/google/secrets", 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
+
 app.get("/login", function (req, res) {
     res.render('login')
-})
+});
 
 app.get("/register", function (req, res) {
     res.render('register')
-})
+});
 
 app.get("/logout", function(req, res) {
     req.logout(function(err) {
@@ -68,7 +118,7 @@ app.get("/secrets", async (req, res) => {
     } else {
         res.redirect('/login');
     }
-})
+});
 
 
 
@@ -76,7 +126,7 @@ app.post("/register", async function (req, res) {
     try {
         await User.register({ username: req.body.username }, req.body.password);
         passport.authenticate("local")(req, res, function () {
-            res.redirect("/secrets");
+            res.redirect("/login");
         });
     } catch (err) {
         console.log("error:", err);
@@ -96,4 +146,4 @@ app.post("/login", passport.authenticate("local", {
 
 app.listen(3000, function () {
     console.log(`Server started on http://localhost:${process.env.PORT}`);
-})
+});
